7d1e4a4730c9db79f13b10bb2029b01c55e7e0a0
mobinanateghii <mobinanateghi@gmail.com>
2021-12-16 22:34:08 +0330

    mapBox

--- ngApp/src/app/_Authentication/Constants.ts
+++ ngApp/src/app/_Authentication/Constants.ts
@@ -1,6 +1,9 @@
 export class Constants {
-  public static REGISTER_URL = 'http://localhost:3000/api/register';
-  public static LOGIN_URL = 'http://localhost:3000/api/login';
-  public static PAYMENT_DATA = 'http://localhost:3000/api/paymentData';
+  public static API_CORE_HOST = 'http://localhost:3000';
+  public static REGISTER_URL = Constants.API_CORE_HOST + '/api/register';
+  public static LOGIN_URL = Constants.API_CORE_HOST + '/api/login';
+  public static PAYMENT_DATA = Constants.API_CORE_HOST + '/api/paymentData';
 
+
+  public static MAP_BOX_TOKEN = 'pk.eyJ1IjoibmF0ZWdoaSIsImEiOiJja3g5NGo3MXMxb2FlMnhvNWExaGtqeWlmIn0.xvEWwiJ7y7k47DumbuniDQ';
 }
--- ngApp/src/app/map/map.component.ts
+++ ngApp/src/app/map/map.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {MapServiceService} from '../_services/map-service.service';
+import * as mapboxgl from 'mapbox-gl';
+import {Constants} from '../_Authentication/Constants';
+import {GeoJSONSource} from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
@@ -8,19 +11,45 @@ import {MapServiceService} from '../_services/map-service.service';
 })
 export class MapComponent implements OnInit {
 
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  location = {lat: 35.72288 , lng: 51.4245497};
+
   constructor(private mapService: MapServiceService) {
   }
 
-  location = {lat: '', lng: ''};
 
   ngOnInit(): void {
     this.getLocation();
+    this.getMapBoxLocation();
   }
 
   getLocation() {
     this.mapService.getLocationService().then(res => {
       this.location.lat = res.lat;
       this.location.lng = res.lng;
+    });
+  }
+
+  getMapBoxLocation(){
+    // @ts-ignore
+    mapboxgl.accessToken = Constants.MAP_BOX_TOKEN;
+    mapboxgl.setRTLTextPlugin(
+      'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
+      null,
+      true // Lazy load the plugin
+    );
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 13,
+      center: [this.location.lng, this.location.lat]
+    });
+
+    this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.on('click' , (event) => {
+      const coordinates = [event.lngLat.lng , event.lngLat.lat];
+      const newMarker = new GeoJSONSource(coordinates);
     });
   }
 
